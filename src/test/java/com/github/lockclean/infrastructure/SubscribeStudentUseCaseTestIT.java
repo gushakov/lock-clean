69663322be4c758745e88ee9502a28c77b7cb7ad
gushakov <Georgy.Ushakov@unil.ch>
2025-02-26 21:18:17 +0100

    use flyway instead of testcontainers

--- src/test/java/com/github/lockclean/infrastructure/SubscribeStudentUseCaseTestIT.java
+++ src/test/java/com/github/lockclean/infrastructure/SubscribeStudentUseCaseTestIT.java
@@ -1,55 +1,32 @@
 package com.github.lockclean.infrastructure;
 
-import com.github.lockclean.infrastructure.adapter.db.course.CourseDbEntity;
-import com.github.lockclean.infrastructure.adapter.db.course.CourseDbEntityRepository;
-import com.github.lockclean.infrastructure.adapter.db.student.StudentDbEntityRepository;
-import com.github.lockclean.infrastructure.adapter.db.subscription.SubscriptionDbEntityRepository;
+import com.github.lockclean.core.model.course.Course;
+import com.github.lockclean.core.model.course.CourseId;
+import com.github.lockclean.infrastructure.adapter.db.PersistenceGateway;
 import com.github.lockclean.infrastructure.adapter.id.JNanoIdGenerator;
 import lombok.AccessLevel;
 import lombok.experimental.FieldDefaults;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jdbc.repository.config.EnableJdbcRepositories;
 import org.springframework.test.annotation.Rollback;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
-import org.testcontainers.utility.DockerImageName;
-
-import java.util.List;
 
 /*
     This is an integration test with uses "slice" technique to bootstrap
-    only the needed Spring Boot Data JDBC infrastructure plus the components
-    scanned from "infrastructure" package and its subpackages.
+    Spring Boot Data JDBC infrastructure. "ComponentScan" annotation will
+    assure that we discover all the components from "infrastructure" package
+    and its sub-packages.
  */
 
-@Testcontainers
 @DataJdbcTest
 @Rollback(value = false)
 @FieldDefaults(level = AccessLevel.PRIVATE)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class SubscribeStudentUseCaseTestIT {
-
-    // Testcontainers container for Postgres
-    @Container
-    private static PostgreSQLContainer<?> pgContainer;
-
-    @BeforeAll
-    static void beforeAll() {
-        // configure and start test container
-        pgContainer = new PostgreSQLContainer<>(DockerImageName.parse("postgres:latest"))
-                .withDatabaseName("lockdb")
-                .withUsername("postgres")
-                .withPassword("postgres")
-                .withReuse(false);
-        pgContainer.setPortBindings(List.of("32779:5432"));
-        pgContainer.start();
-    }
 
     @SpringBootConfiguration
     @EnableJdbcRepositories
@@ -59,50 +36,26 @@ class SubscribeStudentUseCaseTestIT {
     }
 
     @Autowired
-    CourseDbEntityRepository courseRepo;
-
-    @Autowired
-    StudentDbEntityRepository studentRepo;
-
-    @Autowired
-    SubscriptionDbEntityRepository subscriptionRepo;
+    PersistenceGateway persistenceGateway;
 
     @Autowired
     JNanoIdGenerator idGenerator;
-
-    @Test
-    void test_setup_ok() {
-
-    }
 
     @Test
     void save_course() {
 
         String courseId = idGenerator.generateNewCourseId().asString();
 
-
-        // create a course DB entity
-        CourseDbEntity newCourse = CourseDbEntity.builder()
-                .id(courseId)
+        // create a course
+        Course course = Course.builder()
+                .id(new CourseId(courseId))
                 .title("Latin 101")
                 .capacity(20)
                 .build();
 
         // save course
-        courseRepo.save(newCourse);
+        persistenceGateway.saveCourse(course);
 
     }
 
-    @AfterAll
-    static void afterAll() {
-        // uncomment to pause execution so that we can inspect the contents of the database
-        try {
-//            Thread.sleep(Duration.ofHours(1));
-        } catch (Exception e) {
-            // ignore
-        }
-        finally {
-            pgContainer.stop();
-        }
-    }
 }
